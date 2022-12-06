9ea778bc7cb4ea2fbd87ff75e470be6a0eac4892
neong0511@gmail.com <neong0511@gmail.com>
2022-12-06 11:28:17 +0900

    error edit

--- openfeign/src/main/java/com/example/openfeign/OpenfeignApplication.java
+++ openfeign/src/main/java/com/example/openfeign/OpenfeignApplication.java
@@ -3,11 +3,12 @@ package com.example.openfeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient//eureka
+@EnableDiscoveryClient//eureka
 @EnableFeignClients//openfeign
 @EnableCircuitBreaker//hystrix
 public class OpenfeignApplication {
